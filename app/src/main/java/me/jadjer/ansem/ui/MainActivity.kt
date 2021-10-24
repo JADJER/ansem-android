2ad87db188eaeaa53356630fdce9a8b6cf6886b0
jadjer <jadjer@ya.ru>
2021-10-25 01:03:54 +0300

    Change deploy version

--- app/src/main/java/me/jadjer/ansem/ui/MainActivity.kt
+++ app/src/main/java/me/jadjer/ansem/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.jadjer.ansem.ui
 
+import android.accounts.AccountManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.jadjer.ansem.R
+import me.jadjer.ansem.data.AccountGeneral
 import me.jadjer.ansem.databinding.ActivityMainBinding
 
 
@@ -19,10 +21,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var accountManager: AccountManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Ansem)
         super.onCreate(savedInstanceState)
+
+        accountManager = AccountManager.get(this)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -33,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         navController = navHostFragment.navController
-        navController.setGraph(R.navigation.main_navigation)
+        navController.setGraph(R.navigation.nav_graph)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.loginFragment || destination.id == R.id.registrationFragment) {
                 navView.visibility = View.GONE
@@ -57,5 +62,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    /**
+     * Return to login fragment if account deleted from system
+     */
+    override fun onStart() {
+        super.onStart()
+
+        val accounts = accountManager.getAccountsByType(AccountGeneral.ACCOUNT_TYPE)
+        if (accounts.isEmpty()) {
+            navController.navigate(R.id.action_global_loginFragment)
+        }
     }
 }
